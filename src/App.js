7cef2c5cf42e30faf5e28672c0581a0acfbe29a7
takinabradley <bradfinney98@gmail.com>
2022-05-23 08:17:41 -0400

    Add code comments to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import UserImg from './components/UserImg/UserImg'
-import UserContact from './components/UserContact/UserContact';
-import UserEducation from './components/UserEducation/UserEducation';
-import UserExperience from './components/UserExperience/UserExperience'
-import appStyles from './styles/app-styles.css'
-import uniqid from 'uniqid'
-import * as uuid from 'uuid'
+import React from "react";
+import UserImg from "./components/UserImg/UserImg";
+import UserContact from "./components/UserContact/UserContact";
+import UserEducation from "./components/UserEducation/UserEducation";
+import UserExperience from "./components/UserExperience/UserExperience";
+import appStyles from "./styles/app-styles.css";
+import uniqid from "uniqid";
+import * as uuid from "uuid";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
+    /*
+      Thought separating state into individual properties might keep whole app
+      from updating, but I guess not. Any State change === rerender, I suppose.
+      
+      img: null,
+      contact: { name: "", phone: "", email: "", profession: "" },
+      education: { school: "", study: "", date: "" },
+      experience: [],
+    }; */
+
       user: {
         img: null,
         contact: { name: '', phone: '', email: '', profession: '' },
@@ -19,20 +29,25 @@ class App extends React.Component {
         experience: []
       },
     }
+    
 
     this.experienceTemplate = () => ({
-          company: '',
-          title: '',
-          tasks: '',
-          startDate: '',
-          endDate: '',
-          id: uniqid()
-    })
+      company: "",
+      title: "",
+      tasks: "",
+      startDate: "",
+      endDate: "",
+      id: uniqid(),
+    });
 
-    this.onChangeUser = this.onChangeUser.bind(this)
+    this.onChangeUser = this.onChangeUser.bind(this);
   }
 
   onChangeUser(field, change) {
+    /* this.setState({
+      [field]: change,
+    }); */
+
     this.setState({
       user: {
         ...this.state.user,
@@ -42,14 +57,16 @@ class App extends React.Component {
   }
 
   render() {
-    const { img, contact, education, experience } = this.state.user
+    const { img, contact, education, experience } = this.state.user;
     return (
       <div className="app">
         <main>
-          <div className='app-top'>
-            <UserImg  img={img} onChangeUser={this.onChangeUser}/>
-            <UserContact  contact={contact} onChangeUser={this.onChangeUser}/>
-            <UserEducation  education={education} onChangeUser={this.onChangeUser}/>
+          <div className="app-top">
+            <UserImg img={img} onChangeUser={this.onChangeUser} />
+            <UserContact contact={contact} onChangeUser={this.onChangeUser} />
+            <UserEducation education={education}
+              onChangeUser={this.onChangeUser}
+            />
           </div>
 
           <div className="app-bottom">
@@ -60,9 +77,8 @@ class App extends React.Component {
             />
           </div>
         </main>
-        
       </div>
-    )
+    );
   }
 }
 
